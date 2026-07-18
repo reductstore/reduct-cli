b57458114eba893cc78316e09abae8523ae5be06
Malhar Vora <mlvora.2010@gmail.com>
2026-07-18 19:11:28 +0530

    Use ResourcePathParser for entry path arg
    Signed-off-by: Malhar Vora <mlvora.2010@gmail.com>

--- src/cmd/write.rs
+++ src/cmd/write.rs
@@ -1,9 +1,9 @@
 use clap::{Arg, Command};
-use regex::Regex;
 
 use crate::{
     context::CliContext,
     io::{reduct::build_client, std::output},
+    parse::{Resource, ResourcePathParser},
 };
 
 pub(crate) fn write_record_cmd() -> Command {
@@ -12,6 +12,7 @@ pub(crate) fn write_record_cmd() -> Command {
         .arg(
             Arg::new("ENTRY_PATH")
                 .help("Full path the entry to write to")
+                .value_parser(ResourcePathParser::new().allow_alias())
                 .required(true),
         )
         .arg(
@@ -25,8 +26,12 @@ pub(crate) fn write_record_cmd() -> Command {
 }
 
 pub(crate) async fn write_handler(ctx: &CliContext, args: &clap::ArgMatches) -> anyhow::Result<()> {
-    let entry_path = args.get_one::<String>("ENTRY_PATH").unwrap().clone();
-    let (alias_or_url, bucket_name, entry_name) = parse_entry_path(&entry_path)?;
+    let entry_path = args.get_one::<Resource>("ENTRY_PATH").unwrap().clone();
+    let (alias_or_url, bucket_name, entry_name) = entry_path.triple()?;
+
+    let entry_name = entry_name
+        .ok_or_else(|| anyhow::anyhow!("ENTRY_PATH must be alias/bucket/path/to/entry"))?;
+
     let payload = args.get_one::<String>("payload").unwrap().clone();
 
     let client = build_client(ctx, &alias_or_url).await?;
@@ -42,22 +47,6 @@ pub(crate) async fn write_handler(ctx: &CliContext, args: &clap::ArgMatches) -> 
     output!(ctx, "Record written to '{}/{}'", bucket_name, entry_name);
 
     Ok(())
-}
-
-fn parse_entry_path(entry_path: &str) -> anyhow::Result<(String, String, String)> {
-    let re = Regex::new(r"^(?P<alias_or_url>[^/]+)/(?P<bucket_name>[^/]+)/(?P<entry_name>.+)$")?;
-    let caps = re.captures(entry_path).ok_or_else(|| {
-        anyhow::anyhow!(
-            "Invalid entry path. Expected ALIAS/BUCKET/ENTRY, got {}",
-            entry_path
-        )
-    })?;
-
-    let alias_or_url = caps.name("alias_or_url").unwrap().as_str().to_string();
-    let bucket_name = caps.name("bucket_name").unwrap().as_str().to_string();
-    let entry_name = caps.name("entry_name").unwrap().as_str().to_string();
-
-    Ok((alias_or_url, bucket_name, entry_name))
 }
 
 #[cfg(test)]
